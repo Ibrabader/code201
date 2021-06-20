069eb4b2721e53557c862ae8d5ec4c62e264076a
ibrabader <ebraheem1996@hotmail.com>
2021-06-20 16:56:38 +0300

    new site

---
+++ index.js
@@ -0,0 +1,35 @@
+
+alert ('Welcome to the Class of 201')
+
+let a = prompt (" are you a cat person ?");
+if ( a =='yes'){
+document.write("Cool")
+alert('cool')}
+else {
+document.write("Okay okay okay we wil move on  ")
+alert('Okay okay okay we wil move on')
+ } 
+ let b = prompt ( " how old are you ?")
+ if ( b >= 18 )
+ { document.write ( "   mature enough, good for you")
+ alert ('mature enough, good for you')}
+ else 
+ { document.write (' you still have time, enjoy it while you can')
+alert(' you still have time, enjoy it while you can')}
+
+let c = prompt ( "are you excited to start this course ?")
+if ( c == 'yes' )
+{ document.write ( "  we expect no less ")
+alert ('  we expect no less ')}
+else 
+{ document.write (' Dont worry , you will enjoy it here  ')
+alert(' Dont worry , you will enjoy it here  ')}
+ let d = prompt (' Do you have any idea about this field ?')
+ if ( d == 'yes'){
+ document.write ( ' even if you dont, we will get you there')
+ alert("even if you dont, we will get you there")}
+ else{ 
+     
+    document.write ( ' we are excited to see you know every thing')
+    alert(' we are excited to see you know every thing')
+ }
